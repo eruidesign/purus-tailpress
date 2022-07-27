241e4a63488d910b2f7468138e70316bb15bfddc
eRui Design <50480764+eruidesign@users.noreply.github.com>
2022-07-27 16:19:51 +0800

    Pages

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,11 +1,16 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php 
+	$shop_page_ID = get_option( 'woocommerce_shop_page_id' );
+	$shop_page = get_post($shop_page_ID);
+?>
 
-	<header class="entry-header mb-4">
-		<?php the_title( sprintf( '<h1 class="entry-title text-2xl lg:text-5xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-		<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>
+
+	<header class="hero min-h-[300px] max-w-[100%] bg-cover" style="background-image: url(<?php echo get_the_post_thumbnail_url($shop_page_ID,'banner-1440x600');?>);">
+		<?php the_title( sprintf( '<h1 class="entry-title lg:max-w-screen-xl mx-auto p-4 text-2xl lg:text-5xl font-extrabold leading-tight mb-1 text-white"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<!--<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>-->
 	</header>
 
 	<div class="entry-content">
+		<?php the_excerpt();?>
 		<?php the_content(); ?>
 
 		<?php
@@ -21,5 +26,3 @@
 			);
 		?>
 	</div>
-
-</article>
