b8d33a572bb86107ee72f6d635fa0d0e630d84fc
eRui Design <50480764+eruidesign@users.noreply.github.com>
2022-08-08 11:34:33 +0800

    Headers

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -5,7 +5,7 @@
 
 
 	<header class="hero min-h-[300px] max-w-[100%] bg-cover flex justify-center items-center" style="background-image: url(<?php echo get_the_post_thumbnail_url($shop_page_ID,'banner-1440x600');?>);">
-		<?php the_title( sprintf( '<h1 class="entry-title lg:max-w-screen-xl mx-auto p-4 text-2xl lg:text-5xl font-extrabold leading-tight mb-1 text-white"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php the_title( sprintf( '<h1 class="entry-title lg:max-w-screen-xl mx-auto p-4 text-4xl lg:text-5xl font-extrabold leading-tight mb-1 text-white"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 		<!--<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>-->
 	</header>
 
