da5cc3c89d645694b02b78def9959f17e026c705
eRui Design <50480764+eruidesign@users.noreply.github.com>
2022-08-08 15:01:41 +0800

    Shop

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -35,7 +35,7 @@ $shop_page = get_post($shop_page_ID);
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="container mx-auto p-8 hero-content text-neutral-content">
         <div class="lg:max-w-screen-xl mx-auto">
-        <div class="py-3 px-5 mb-4 bg-white text-black max-w-auto rounded-md border border-primary" role="alert">
+        <div class="py-3 px-5 mb-4 bg-white text-black max-w-lg text-sm rounded-md border border-primary flex items-center justify-between" role="alert">
             <span>For orders over 50 please <a class="text-primary" href="<?php echo get_permalink(14); ?>">Contact Us</a>!</span>
             <button class="w-4" type="button" data-dismiss="alert" aria-label="Close" onclick="this.parentElement.remove();">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
