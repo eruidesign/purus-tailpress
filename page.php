3900293bad775d9eee4b8728bd695763d8aa4f3f
eRui Design <50480764+eruidesign@users.noreply.github.com>
2022-08-08 11:40:23 +0800

    Breadcrumbs

--- page.php
+++ page.php
@@ -1,13 +1,6 @@
 <?php get_header(); ?>
 
 	<div class="">
-		<div class="bg-zinc-900">
-			<?php
-				if ( function_exists('yoast_breadcrumb') ) {
-				yoast_breadcrumb( '<div id="breadcrumbs" class="lg:max-w-screen-xl p-4 mx-auto text-gray-500">','</div>' );
-				}
-			?>
-		</div>
 
 	<?php if ( have_posts() ) : ?>
 
